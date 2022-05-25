82634324065cd2c19aeb244cb4fd60526171d32e
Ymuzhych <myuliia2018@gmail.com>
2022-05-24 21:00:45 -0500

    complete markdown for readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,4 +2,38 @@
 function generateMarkdown(data) {
     return `
     # ${data.title}
+
+    https://github.com/${data.Username}/${data.Title}
+
+    # Description
+    ${data.Description}
+    
+    # Table of Contents
+
+    * [Installation](#instalation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    # Installation
+
+    The following necessary dependencies must be installed to run the application properly: inquire, fs, util 
+    
+    # Usage 
+    To use this app, ${data.Usage}
+
+    # License
+    This project is licensed under the ${data.License} license.
+
+    # Contributing 
+    Contributing: ${data.Contributing}
+
+    # Tests
+    To run the test you need: ${data.Tests}
+
+    # Questions
+    If you have any questions contact ${data.Username}
+
     `}
+    module.exports = generateMarkdown;
